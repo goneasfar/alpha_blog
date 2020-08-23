360b8e9a478d23b72ad8a6b457efad634c460fef
alex <alex.macneil@hotmail.com>
2020-08-23 12:11:22 -0500

    Added one to many relationship between users and articles.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class EmailValidator < ActiveModel::EachValidator
 end
 
 class User < ApplicationRecord
+  has_many :articles
   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25 }
   validates :email, presence: true, uniqueness: {case_sensitive: false}, email: true, length: {maximum: 255}
 end
