7a06628e9ce96cb38750a67fbd6fd17d90a527bc
alex <alex.macneil@hotmail.com>
2020-08-24 07:51:07 -0500

    Added password digest. Added password column to all users

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,5 @@ class User < ApplicationRecord
   has_many :articles
   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25 }
   validates :email, presence: true, uniqueness: {case_sensitive: false}, email: true, length: {maximum: 255}
+  has_secure_password
 end
